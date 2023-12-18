00d64979149ea034d387b9416ac8c3a70291c0b6
Shoetan <emmysoetan@gmail.com>
2023-12-18 21:47:23 +0100

    feat: added jwt verification

--- routers/loginusers.route.go
+++ routers/loginusers.route.go
@@ -10,7 +10,7 @@ func LoginUser (router *gin.Engine) {
 
 	// Add cors middleware
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:[]string{ "http://localhost:9999"     },
+		AllowOrigins:[]string{ "http://localhost:9999" },
 		AllowMethods: []string{"POST"},
     AllowHeaders: []string{"Content-Type, Authorization"},
 	}))
